df86a7022c5361e93e634a700a0c0941a1f74791
David Chuvik <dchuvik@gmail.com>
2023-02-09 12:02:35 -0500

    greet's names that are supplied by the user

---
+++ ForLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿string firstName = string.Empty;
+List<string> firstNames = new List<string>();
+
+Console.WriteLine("Please enter a comma seperated list of first names: ");
+string firstNameList = Console.ReadLine();
+
+string[] ArrayOfFirstNames = firstNameList.Split(',').ToArray();
+
+for (int i = 0;i < ArrayOfFirstNames.Length; i++) 
+    {
+        Console.WriteLine($"Hello {ArrayOfFirstNames[i]}");
+    }
+
+
+
